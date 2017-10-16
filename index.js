dd6e6fc0ddc767890e86999237d80ffa61b190aa
Jan Zagata <refuse@inuits.eu>
2017-10-16 12:20:34 +0200

    controller methods

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-var application = require("application");
-var context = application.android.context;
+var InuitsMqtt = /** @class */ (function () {
+    function InuitsMqtt() {
+        this.connector = null;
+        var application = require("application");
+        var context = application.android.context;
+        this.connector = new eu.inuits.android.mqttlib.InuitsMqttControler();
+        this.connector.init(context);
+    }
+    InuitsMqtt.prototype.connect = function (uri) {
+        this.connector.connect(uri);
+    };
+    InuitsMqtt.prototype.disconnect = function () {
+        return this.connector.disconnect();
+    };
+    InuitsMqtt.prototype.subscribe = function (topic) {
+        var application = require("application");
+        var intent = this.connector.subscribe(topic);
+        console.log(intent);
+        
+        return this.connector.subscribe(topic);
+    };
+    InuitsMqtt.prototype.unsubscribe = function (topic) {
+        return this.connector.unsubscribe(topic);
+    };
+    InuitsMqtt.prototype.publish = function (topic, message) {
+        return this.connector.publish(topic, message);
+    };
+
+    return InuitsMqtt;
+}());
 
 module.exports = {
-    startIntent: function() {
-        var mqttControler = new eu.inuits.android.mqttlib.InuitsMqttControler();
-        mqttControler.start(context);
-    }
-};
+    inuitsMqtt: InuitsMqtt
+}
